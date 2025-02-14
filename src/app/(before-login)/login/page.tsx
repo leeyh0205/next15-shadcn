744d2eda0d43ae4fbccc75ec9f811e8c2869fc23
이영환 <dowk3432@gmail.com>
2025-02-14 15:13:25 +0900

    [Feat]: Login

---
+++ src/app/(before-login)/login/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { LoginForm } from "./_components/login-form";
+
+const page = () => {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <LoginForm />
+      </div>
+    </div>
+  );
+};
+
+export default page;
