728361d1a6fba5f4071418418b70836b381b3238
이영환 <dowk3432@gmail.com>
2025-02-14 15:06:18 +0900

    [Fix]: Contents

--- src/app/(login)/layout.tsx
+++ src/app/(login)/layout.tsx
@@ -7,13 +7,15 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { Separator } from "@/components/ui/separator";
 import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
-export default function Page() {
+
+const Layout = ({ children }: React.PropsWithChildren) => {
   return (
     <SidebarProvider>
       <AppSidebar />
@@ -37,15 +39,20 @@ export default function Page() {
             </Breadcrumb>
           </div>
         </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+        {/* <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
           <div className="grid auto-rows-min gap-4 md:grid-cols-3">
             <div className="aspect-video rounded-xl bg-muted/50" />
             <div className="aspect-video rounded-xl bg-muted/50" />
             <div className="aspect-video rounded-xl bg-muted/50" />
           </div>
           <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
-        </div>
+        </div> */}
+        <ScrollArea className="h-[calc(100dvh-64px)] min-w-[700px] overflow-auto bg-background-normal p-4">
+          {children}
+        </ScrollArea>
       </SidebarInset>
     </SidebarProvider>
   );
-}
+};
+
+export default Layout;
